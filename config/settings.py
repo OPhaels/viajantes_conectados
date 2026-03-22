79faa8420104f90114fbc6d808f716881f854bf1
Raphael Rodrigues Marçal <104911531+OPhaels@users.noreply.github.com>
2026-03-22 04:11:59 -0300

    fix: saves images

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL  = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/mnt/media/'
 
 # Configurações de Segurança
 if not DEBUG:
